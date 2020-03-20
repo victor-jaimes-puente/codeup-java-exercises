a9a9f0674266cb1219be9c80f571a6155bddb50a
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-20 14:58:15 -0500

    Finished do while loop2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,12 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        Do While
-//
-//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-//        Alter your loop to count backwards by 5's from 100 to -10.
-//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 
-        int i = 5;
+
+        long i = 2;
         do {
 
             System.out.print(" " + i);
-            i++;
-        } while (i < 15);
+            i*=i;
+        } while (i <= 1000000);
     }
 }
