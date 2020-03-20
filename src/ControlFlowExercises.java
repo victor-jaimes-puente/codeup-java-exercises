724bc31f0ec1c591cddf3dd17426f4b5eac6938a
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-20 15:44:19 -0500

    Finished FIzzBuzz in multiple ways

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,31 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//test
-//test2
-        long i = 2;
-        do {
+//        Fizzbuzz
+//
+//        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+//                Write a program that prints the numbers from 1 to 100.
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
 
-            System.out.print(" " + i);
-            i*=i;
-        } while (i <= 1000000);
+        // 1 print from 1 to 100
+        // 2 multiples of 3 print Fizz instead of number
+        // 3 multiples of 5 print Buzz instead of number
+        // 4 multiples of both print FizzBuzz instead of number
+
+        for (long i = 1; i < 101; i++) {
+        if ((i%3 == 0)&&(i%5==0)){
+            System.out.println("( "+ i + " FIZZBUZZ )");
+            } else if (i%3 == 0){
+            System.out.println("( "+ i + " Fizz )");
+        }else if (i%5==0){
+            System.out.println("( "+ i + " Buzz )");
+        }
+        else{
+            System.out.println(i);
+        }
+        }
     }
 }
