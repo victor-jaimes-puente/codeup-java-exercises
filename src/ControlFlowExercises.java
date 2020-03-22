5c1186a3df2eeb08e928035f62f91e2928045a20
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-22 12:41:11 -0500

    updating Tree

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -17,7 +17,7 @@ public class ControlFlowExercises {
 
         for (long i = 1; i < 101; i++) {
         if ((i%3 == 0)&&(i%5==0)){
-            System.out.println("( "+ i + " FIZZBUZZ )");
+            System.out.println(" FIZZBUZZ");
             } else if (i%3 == 0){
             System.out.println("( "+ i + " Fizz )");
         }else if (i%5==0){
