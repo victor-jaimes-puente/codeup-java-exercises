2f9b59457ff5ab2f5876a9b8205cabb61a9f5b15
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-23 11:21:00 -0500

    Working on second half

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,20 @@
 public class StringExercise {
     public static void main(String[] args){
-        System.out.println("We don't need no education");
-        System.out.println("We don't need no thought control");
-        System.out.println("Check \"this\" out!, \"s inside of \"s! ");
-        System.out.println("In windows, the main drive is usually C:\\");
-        System.out.println("I can do backslashes \\, double backslashes \\\\ and the amazing tirple backslash \\\\\\!");
+//        System.out.println("We don't need no thought control");
+//        System.out.println("Check \"this\" out!, \"s inside of \"s! ");
+//        System.out.println("In windows, the main drive is usually C:\\");
+//        System.out.println("I can do backslashes \\, double backslashes \\\\ and the amazing tirple backslash \\\\\\!");
+//        System.out.println();
+        String Education = ("We don't need no education");
+        System.out.println(Education);
+        // need to practice checking the specific value of a string like a specific letter or char
+        int lengthOfEdu = Education.length();
+        System.out.println(lengthOfEdu);
+        char lastOfEdu = Education.charAt(25);
+        System.out.println(lastOfEdu);
+        int indexOfD = Education.lastIndexOf("d");
+        System.out.println(indexOfD);
+
+
     }
 }
