839f6a0e7037ec68c312964802540c0fd5b3220d
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-24 11:37:41 -0500

    updating tree for morning

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,7 +5,7 @@ public class StringExercise {
 //        System.out.println("In windows, the main drive is usually C:\\");
 //        System.out.println("I can do backslashes \\, double backslashes \\\\ and the amazing tirple backslash \\\\\\!");
 //        System.out.println();
-        String Education = ("We don't need no education");
+        String Education = ("  We don't need no education  ");
         System.out.println(Education);
         // need to practice checking the specific value of a string like a specific letter or char
         int lengthOfEdu = Education.length();
@@ -14,7 +14,21 @@ public class StringExercise {
         System.out.println(lastOfEdu);
         int indexOfD = Education.lastIndexOf("d");
         System.out.println(indexOfD);
+        String Replaced = Education.replace("We", "They");
+        System.out.println(Replaced);
+        System.out.println(Education);
+        String word = Education.substring(0,8);
+        System.out.println(word);
+        String CAPITlAIZED = Replaced.toUpperCase();
+        System.out.println(CAPITlAIZED);
+        String Trimmed = Replaced.trim().toUpperCase();
+        System.out.println(Trimmed);
 
 
-    }
-}
+
+
+
+
+
+
+
