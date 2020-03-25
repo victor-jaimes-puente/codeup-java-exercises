6e79439f81bb5dec929ec29191da6706870cbec0
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-25 10:13:30 -0500

    updating tree

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,16 +1,63 @@
+import javax.swing.*;
+
 public class MethodsExercises {
+    //    MAIN
     public static void main(String[] arg) {
-        System.out.println();
-        System.out.println(sayHello("Victor", 27));
+//        System.out.println();
+//        System.out.println(sayHello("Victor", 27));
+////        count(5);
+//        System.out.println("Doing some adding "+add(4, 4));
+//        System.out.println("Doing some multiplying "+multiply(4, 10));
+//        System.out.println("Doing some deviding "+devide(4, 4));
+//        System.out.println("Doing some subtracting "+subtract(4, 4));
+
     }
 
-    public static String sayHello(String name) {
+    //END OF MAIN
+//    public static String sayHello(String name) {
+//
+//        return String.format("Hello " + name);
+//    }
+//
+//    //    VERSION.2
+//    public static String sayHello(String name, Integer age) {
+//        return String.format("Hello, I am " + name + "\nI am " + age + " years old");
+//    }
+//
+//    //    ANOTHER.TEST
+////    public static void count(int n) {
+////        if (n <= 0) {
+////            System.out.println("All done!");
+////            return;
+////        }
+////        System.out.println(n);
+////        count(n - 1);
+////    }
+//
+//    // ADDITION METHOD
+//    public static int add(int a, int b) {
+//        System.out.printf("\nInput A %s\nInput B %s\n", a , b);
+//        return a + b;
+//    }
+//
+//    // MULTIPLICATION METHOD
+//    public static int multiply(int a, int b) {
+//        System.out.printf("\nInput A %s\nInput B %s\n", a , b);
+//       return a*b;
+//    }
+//
+//    // DEVISION METHOD
+//    public static int devide(int a, int b) {
+//        System.out.printf("\nInput A %s\nInput B %s\n", a , b);
+//        return a / b;
+//    }
+//
+//    // SUBTRACTION METHOD
+//    public static int subtract(int a, int b) {
+//        System.out.printf("\nInput A %s\nInput B %s\n", a , b);
+//        return a - b;
+//    }
 
-        return String.format("Hello " + name);
-    }
-
-    //    VERSION.2
-    public static String sayHello(String name, Integer age) {
-        return String.format("Hello, I am " + name + "\nI am " + age + " years old");
-    }
+//SECTION 4 DICE
+    
 }
