27f5b6c970e4bca1a37c96f7fa82e9bf1896c8e6
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-03 02:54:56 -0500

    updating tree

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,16 +1,55 @@
+import java.nio.file.LinkPermission;
+import java.util.Arrays;
 import javax.sound.midi.Soundbank;
 import java.sql.SQLOutput;
-import java.util.Arrays;
 
 public class ArraysExercises {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
-        String[] Person = new String[3];
-        Person[0]="Alpha";
-        Person[1]="Bravo";
-        Person[2]="Charlie";
-        System.out.println(Arrays.toString(Person));
+        public static String addPerson (String[]inputPeople, String inputPerson){
+            int[] numbers = {1, 2, 3, 4, 5};
+            System.out.println(Arrays.toString(numbers));
+            String[] people = new String[3];
+
+            people[0] = new String("Alpha");
+            people[1] = new String("Bravo");
+            people[2] = new String("Charlie");
+
+            for (int i = 0; i < people.length; i++) {
+                System.out.println(people[i]);
+            }
+
+            for (String person : people) {
+                System.out.println(person);
+            }
+            System.out.println();
+
+
+//            for (int)
+            String addition = new String("Alpha");
+            for (String person : addPerson) {
+                System.out.println("\t" + addition);
+            }
+                return addition;
+        }
+            String[] updatePeople = addPerson(people, addition);
+
     }
 }
+
+// Create static method named addPerson
+//(P1) Accepts array of Persons && (P2)single person object
+// Input will be added to the passed array
+// TWO PARAMETERS, P1 AND P2
+//should return an array whose length is 1 greater than the passed array, with the passed person object at
+// the end of the array.
+
+
+
+
+
+
+
+
+
+
