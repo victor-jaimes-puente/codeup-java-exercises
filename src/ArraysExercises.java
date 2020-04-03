7d8afc52914ca3d9d83785150e268c39d44ad394
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-02 23:26:32 -0500

    Began Array Exercise

---
+++ src/ArraysExercises.java
@@ -0,0 +1,16 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+        String[] Person = new String[3];
+        Person[0]="Alpha";
+        Person[1]="Bravo";
+        Person[2]="Charlie";
+        System.out.println(Arrays.toString(Person));
+    }
+}
