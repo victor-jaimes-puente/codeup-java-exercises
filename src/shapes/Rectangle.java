b88580536de983d9ae9788e5da710dabfc729cb3
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-19 19:22:16 -0500

    updating tree

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,5 +1,28 @@
-package shapes;
-import java.util.Scanner;
+public class Rectangle{
+    protected static int length( int length){
+        return length;
+    }
+    protected static int width( int width){
+        return width;
+    }
+    protected static double getArea(int lenght, int width) {
+        return lenght * width;
+    }
+    protected static double getPerimiter(int length, int width){
+            return 2*length + 2*width;
+        
 
-public class Rectangle {
+    }
+//SQUARE CLASS
+     class Square extends Rectangle{
+
+}
+//    MAIN
+public static void main(String[] args) {
+    System.out.println(length(4));
+    System.out.println(width(4));
+    System.out.println(getArea(length(4), width(4)));
+    System.out.println(getPerimiter(length(5),width(10)));
+}
+
 }
