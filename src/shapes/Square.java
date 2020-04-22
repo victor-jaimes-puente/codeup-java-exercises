c070c2266a2b8b789075bef49c8647432d82abb3
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-22 12:35:59 -0500

    finished first part of square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,17 @@
 package shapes;
 
 public class Square {
+    protected double side;
 
+    public Square(double side){
+        this.side = side;
+    }
+    public double getArea(){
+        return Math.pow(this.side, 2);
+    }
+    public double getPerimeter(){
+        return 4 * this.side;
+    }
 //MAIN
 public static void main(String[] args) {
 
