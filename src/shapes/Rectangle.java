2688b5ef934e0502e0aaf76044af8bbcedfadc4b
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-22 12:39:17 -0500

    updating tree

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,19 @@
 package shapes;
 
 public class Rectangle {
-    protected double length;
-    protected double width;
-
-    public  Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
-    }
-    public double getArea() {
-        return this.length * this.width;
-    }
-    public double getPerimiter(){
-        return  this.length * this.width;
-    }
+//    protected double length;
+//    protected double width;
+//
+//    public  Rectangle(double length, double width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//    public double getArea() {
+//        return this.length * this.width;
+//    }
+//    public double getPerimiter(){
+//        return  this.length * this.width;
+//    }
 //    MAIN
 public static void main(String[] args) {
 
