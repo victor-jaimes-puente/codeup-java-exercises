c9757d23e3a984377a4a937396c89c0922330da4
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-22 13:53:57 -0500

    updatingTree

---
+++ src/grades/Student.java
@@ -0,0 +1,72 @@
+
+package grades;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    public static void main(String[] args) {
+        Student bob = new Student("Bob");
+        bob.addGrade(25);
+        bob.addGrade(50);
+        bob.addGrade(75);
+        bob.addGrade(100);
+
+        System.out.println(bob.getName() +  "'s grade point average: " + bob.getGradeAverage());
+    }
+
+    private String name;
+    private ArrayList<Integer> grades;
+    private HashMap<String, String> attendance;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+        this.attendance = new HashMap<>();
+    }
+
+    // returns the student's name
+    public String getName() {
+        return this.name;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        int sum = 0;
+
+        for(int grade : this.grades) {
+            sum += grade;
+        }
+
+        return (double) sum / this.grades.size();
+    }
+
+    public void recordAttendance(String date, String value) {
+        this.attendance.put(date, value);
+    }
+
+    public HashMap<String, String> getAttendance() {
+        return this.attendance;
+    }
+
+    public double attendancePercentage() {
+        int absences = 0;
+
+        for(String a : this.attendance.values()) {
+            if(a.equals("A")) {
+                absences++;
+            }
+        }
+
+        return ((double) (this.attendance.size() - absences) / this.attendance.size()) * 100;
+    }
+}
--- src/shapes/ShapeTest.java
+++ src/shapes/ShapeTest.java
@@ -4,11 +4,25 @@ abstract class ShapesTest {
 
     public static void main(String[] args) {
 
+        Measurable myShape;
 
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
-        System.out.println("box1.getArea() = " + box1.getArea());
+        myShape = new Square(5);
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
 
+        myShape = new Rectangle(4, 6);
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+
+
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
+//        System.out.println("box1.getArea() = " + box1.getArea());
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
+//        System.out.println("box2.getArea() = " + box2.getArea());
 
     }
+
 }
